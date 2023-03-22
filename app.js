3ba7f942508ed4449b7e95f2659cef9ef477665c
Iago Luan <101263608+Iagolbdc@users.noreply.github.com>
2023-03-22 18:07:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -19,37 +19,60 @@ function Cadastrar(){
     localStorage.setItem('cadastrados', JSON.stringify(usuarios))
     location.assign('login.html')
     }
-    if(nome.length > 2 || nome.trim().length != 0 ){
-        p[0].innerHTML = 'O nome precisa ter ao menos 3 caracters'
+    console.log(nome.trim().length)
+    if(nome.length <= 2 || nome.trim().length === 0 ){
+        p[0].innerHTML = 'O nome precisa ter ao menos 3 caracteres'
         setTimeout(()=>{
             p[0].innerHTML = ''
         }, 3000)
     }
-    if(senha.length > 2 || senha.trim().length != 0 ){
-        p[1].innerHTML = 'O nome precisa ter ao menos 3 caracters'
+    if(senha.length < 8 || senha.trim().length == 0 ){
+        p[1].innerHTML = 'A senha precisa ter ao menos 8 caracteres'
         setTimeout(()=>{
             p[1].innerHTML = ''
         }, 3000)
     }
+    
 }
 
 function Login(){
+    let nome = document.getElementById('Nome').value
+    let senha = document.getElementById('Senha').value
+
     let string = localStorage.getItem('cadastrados')
     console.log(JSON.parse(string))
+
+    if(nome.length <= 2 || nome.trim().length === 0 || senha.length < 8 || senha.trim().length == 0 ){
+        p[0].innerHTML = 'Usuário ou senha incorretos!'
+        setTimeout(()=>{
+            p[0].innerHTML = ''
+        }, 3000)
+    }
 }
 
 
-/*
-let arr = ['asda', 'adfasda', 'sdfsdf']
+// let arr = ['asda', 'adfasda', 'sdfsdf', 'aksugdja', 'jahsdkjashb']
 
-arr.forEach(item=>{
-    document.write(item + "<br>")
-})
+// let variavel = 'jahsdkjashb'
 
-let posicao = arr.findIndex((item) =>{
-    return item 
-})
+// let count = 0
+// let count2 = 0
 
-document.write(posicao)
+// arr.forEach(item=>{
+//     //document.write(item + "<br>")
+//     if(item == variavel){
+//         count2 = count
+//     }
+//     count++
+// })
 
-*/
+// console.log(count2)
+
+
+// // let posicao = arr.findIndex((item) =>{
+// //     console.log(item)
+// //     return item = 'adfasda'
+// // })
+
+// // document.write(posicao)
+
